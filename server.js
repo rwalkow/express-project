609619c77b2a9a26eedf851b99c914238be821cc
Rafal Walkow <r.walkow@po.edu.pl>
2022-08-05 12:44:26 +0200

    Add public components

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('/', (req, res) => {
   res.show('index.html');
 });
@@ -28,6 +30,10 @@ app.get('/info', (req, res) => {
 
 app.get('/history', (req, res) => {
   res.show('history.html');
+});
+
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '/style.css'));
 });
 
 app.listen(8000, () => {
